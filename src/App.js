5b0d65b8f19b3dbf3d5eaa3725b871334a85a6b0
marvinorias <oriasmarvinm@gmail.com>
2022-06-16 22:19:17 +0800

    trial_1 innitial commit for today

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
+const Person = (props) => {
+  return(
+  <>
+    <h1>Name: {props.name}</h1>
+    <h2>Last Name: {props.lName}</h2>
+    <h2>Age: {props.age}</h2>  
+  </>
+  )
+};
+
 function App() {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    alert(`You have loaded the page`)
+  }, [value]);
+  
+  const addValue = () =>{
+    setValue(value+1);
+  }
+  function minusValue(){
+    if(value === 0){
+      alert(`Limit reached`)
+    }
+    else{
+      setValue((prevCount)=>(
+          prevCount - 1
+        ))
+    }
+  }
+
+  const name = 'Jack';
+  const name1 = null;
+  const isNameShowing = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React!</h1>
+      <h3>Hello {isNameShowing ? name : `Guest`}!</h3>
+      {name1 ? (
+      <>
+        test  
+      </>): (
+        <>
+          <h2>test</h2>
+          <h1>No name</h1>
+        </>
+      )}
+      <Person name={`Jane`} lName="Doe" age={30}/>
+      <Person name={`John`} lName="Doe" age={25}/>
+      <Person name={`Marvin`} lName="Orias" age={25}/>
+      <Person name={`Angela Greta`} lName="Matugas" age={21}/>
+      <Person name={`Atarah Elisha`} lName="Macaraig" age={4}/>
+      <button onClick={addValue}>INCREASE(+)</button>
+      <h1>{value}</h1>
+      <button onClick={minusValue}>DECREASE(-)</button>
     </div>
   );
 }
